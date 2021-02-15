8d300b3cea0d59d257919c83ef8c41b2fba00373
ponyjackal <pony1125@outlook.com>
2021-02-15 04:48:43 -0500

    make Footer reponsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,16 @@ import { Link } from "react-router-dom";
 
 const cStyles = {
   footer:
-    "flex flex-col justify-between bg-gray-900 text-gray-300 py-10 pl-56 pr-10 space-y-10",
+    "flex flex-col justify-between bg-gray-900 text-gray-300 py-10 pl-10 md:pl-36 lg:pl-56 pr-10 space-y-10",
   footerTop: "flex flex-col justify-between space-y-3",
-  menu: "flex flex-row justify-start space-x-4",
+  menu: "grid grid-cols-3 md:flex md:flex-row justify-start space-x-4",
   menuItem: "border-r-2 pr-4 border-gray-300",
-  footBottom: "flex felx-row justify-between items-center py-5",
+  footBottom:
+    "flex flex-col justify-start item-start space-y-5 md:flex-row md:justify-between md:items-center md:space-y-0 py-5",
   socialContainer: "flex felx-row justify-start space-x-4",
-  socialIcon: "w-10 h-10",
+  socialIcon: "w-7 h-7 md:w-9 md:h-9 lg:w-10 lg:h-10",
   storeContainer: "flex felx-row justify-end items-center space-x-4",
-  storeIcon: "h-10",
+  storeIcon: "h-7 md:h-9 lg:h-10",
 };
 
 const Footer = () => {
